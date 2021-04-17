1624c7c51dfeef2f8d35578c22d0a0d1e3fe68ae
SilLee0465 <61728125+SilLee0465@users.noreply.github.com>
2021-04-17 12:54:02 +0800

    Added Door

---
+++ VR F/Assets/DoorGrabbable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorGrabbable : OVRGrabbable
+{
+    public Transform handler;
+    public override void GrabEnd(Vector3 linearVelocity, Vector3 angularVelocity)
+    {
+        base.GrabEnd(Vector3.zero, Vector3.zero);
+        transform.position = handler.transform.position;
+        transform.rotation = handler.transform.rotation;
+
+        Rigidbody rbhnandler = handler.GetComponent<Rigidbody>();
+        rbhnandler.velocity = Vector3.zero;
+        rbhnandler.angularVelocity = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(handler.position, transform.position) > 0.6f)
+        {
+            grabbedBy.ForceRelease(this);
+        }
+    }
+}
---
+++ VR F/Assets/FollowPhysics.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPhysics : MonoBehaviour
+{
+    public Transform target;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(target.transform.position);
+    }
+}
