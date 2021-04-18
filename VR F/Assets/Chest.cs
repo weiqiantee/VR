2510598e5826ff02b0e3bf77cbea2fde4b62b29f
SilLee0465 <61728125+SilLee0465@users.noreply.github.com>
2021-04-18 22:56:27 +0800

    Added Chest Funciton
    Major
    - Player can throw coin into the chest

---
+++ VR F/Assets/Chest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chest : MonoBehaviour
+{
+    public int totalCoinCount;
+    int curCoinCount;
+
+    private void Start()
+    {
+        curCoinCount = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Coin" && curCoinCount <= totalCoinCount)
+        {
+            curCoinCount++;
+            Destroy(other.gameObject);
+            Debug.LogError(curCoinCount);
+        }
+        else
+        {
+            //Win condition here
+            Debug.Log("You have won");
+        }
+    }
+}
